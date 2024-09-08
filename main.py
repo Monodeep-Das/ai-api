30e82ce6df26d5a996217e4e130c6aaeb3514e01
Monodeep-Das <m.monodas@gmail.com>
2024-09-09 03:26:13 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+# In your main.py file
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app import process_data
 
 app = FastAPI()
+
+# Allowing CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can specify your front-end domain instead of "*"
+    allow_credentials=True,
+    allow_methods=["*"],  # This allows all methods (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],  # This allows all headers
+)
 
 app.include_router(process_data)
 
